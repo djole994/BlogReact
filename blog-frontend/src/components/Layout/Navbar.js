f6cac820228c390fd225c6e1dbfaf0ebb1ce788a
djole994 <djordjeradovic94@gmail.com>
2025-03-12 19:51:15 +0100

    s

--- blog-frontend/src/components/Layout/Navbar.js
+++ blog-frontend/src/components/Layout/Navbar.js
@@ -4,17 +4,36 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light px-3">
-      <Link className="navbar-brand" to="/">My Blog</Link>
-      <div className="collapse navbar-collapse">
+      <Link className="navbar-brand" to="/">
+        My Blog
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav ms-auto">
           <li className="nav-item">
-            <Link className="nav-link" to="/login">Login</Link>
+            <Link className="nav-link" to="/login">
+              Loginsss
+            </Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/register">Register</Link>
+            <Link className="nav-link" to="/register">
+              Register
+            </Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/posts">Posts</Link>
+            <Link className="nav-link" to="/posts">
+              Posts
+            </Link>
           </li>
         </ul>
       </div>
